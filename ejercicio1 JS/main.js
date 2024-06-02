5fbbb15a7016634cc3f517513d690b827f6ebb10
EmanuelVargas11 <vargasriquelmege@gmail.com>
2024-06-02 00:19:13 -0300

    Se realizo ejercicios de JS

---
+++ ejercicio1 JS/main.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+// ***************************************************************//
+// ************************Ejercicio 1:***************************//
+// ***************************************************************//
+ 
+/*
+Crea un programa que solicite al usuario su nombre y apellido. Luego, muestra
+una alerta con su nombre completo (nombre y apellido concatenados) utilizando
+template strings 
+*/
+
+alert("Hola Visitabte")
+let ingresoNombre = prompt("¿cual es tu nombre?");
+let ingresoApellido = prompt("¿cual es tu apellido?");
+
+console.log(ingresoNombre)
+console.log(ingresoApellido)
+
+alert(`Gracias por visitarnos, ${ingresoNombre} ${ingresoApellido}, hasta pronto!!!`);
+
+
+
---
+++ ejercicio2 JS/main.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+// ***************************************************************//
+// ************************Ejercicio 2:***************************//
+// ***************************************************************//
+ 
+/*
+Pide al usuario que ingrese una frase. Luego, muestra en la consola la longitud
+de la frase utilizando template strings. 
+*/
+
+
+let ingresoFrase = prompt('Hola que tal, por favor ingrese una frase ')
+
+let longitud = (`La longitud de la frase es: ${ingresoFrase.length}`)
+
+console.log(longitud)
+
+
+
